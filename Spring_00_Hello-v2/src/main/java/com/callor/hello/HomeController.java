6b9253041514731d76b13c8cfca706360e93f1b7
inqu0302 <inqu0302@naver.com>
2021-11-02 16:42:55 +0900

    인텔리제이로 스프링mvc 작성하기

---
+++ Spring_00_Hello-v2/src/main/java/com/callor/hello/HomeController.java
@@ -0,0 +1,4 @@
+package com.callor.hello;
+
+public class HomeController {
+}
---
+++ Spring_00_Hello-v4/src/main/java/com/callor/hello/MainController.kt
@@ -0,0 +1,18 @@
+package com.callor.hello
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class MainController {
+
+    @ResponseBody
+    @RequestMapping(value=["/main"], method=[RequestMethod.GET])
+    fun main() :String{
+
+        return "코틀린 컨트롤러"
+    }
+
+}
